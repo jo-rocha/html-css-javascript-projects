fce28f3b7ed362aea0fe39b939c22438b305215c
jo-rocha <jope.orocha@gmail.com>
2022-01-24 15:58:19 -0300

    adding the text area and the number to the range slider

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var slider = document.getElementById("satisfaction-slider");
-var output = document.getElementById("demo");
+var output = document.getElementById("value");
 output.innerHTML = slider.value;
 
 slider.oninput = function(){
-    output.innerHTML - this.value;
+    output.innerHTML = this.value;
 }
